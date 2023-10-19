3dffe7d7beb9e1ebae455af38a097502760ffa0f
abeade <albert.beade@gmail.com>
2023-10-19 18:05:24 +0200

    Remove android studio dependency

--- src/main/kotlin/com/abeade/plugin/figma/utils/AnActionEventExtensions.kt
+++ src/main/kotlin/com/abeade/plugin/figma/utils/AnActionEventExtensions.kt
@@ -8,32 +8,30 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.DumbService
-import com.intellij.openapi.project.IndexNotReadyException
 import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.kotlin.idea.base.util.isAndroidModule
-import kotlin.math.min
 
 fun AnActionEvent.getMinSdkVersion(): Int {
     val module: Module? = getData(LangDataKeys.MODULE) ?: getData(LangDataKeys.MODULE_CONTEXT)
-    var minSdkVersion = Int.MAX_VALUE
+    var minSdkVersion: Int = Int.MAX_VALUE
     if (module != null && module.isAndroidModule()) {
         val facet = AndroidFacet.getInstance(module)
         if (facet != null) {
             val minSdk = getMinSdkVersion(facet)
-            minSdkVersion = min(minSdkVersion.toDouble(), minSdk.featureLevel.toDouble()).toInt()
+            minSdkVersion = minSdk?.featureLevel ?: Int.MAX_VALUE
         }
     }
     return minSdkVersion
 }
 
-private fun getMinSdkVersion(facet: AndroidFacet): AndroidVersion {
-    val androidModel = AndroidModel.get(facet)
-    if (androidModel != null) {
-        return androidModel.minSdkVersion
-    }
-    try {
-        return DumbService.getInstance(facet.module.project)
+private fun getMinSdkVersion(facet: AndroidFacet): AndroidVersion? = AndroidModel.get(facet)?.minSdkVersion
+    ?: try {
+        DumbService.getInstance(facet.module.project)
             .runReadActionInSmartMode<AndroidVersion> { facet.queryMinSdkAndTargetSdkFromManifestIndex().minSdk }
-    } catch (_: IndexNotReadyException) { }
-    return MergedManifestManager.getMergedManifestSupplier(facet.module).get().get().minSdkVersion
-}
+    } catch (_: Exception) {
+        try {
+            MergedManifestManager.getMergedManifestSupplier(facet.module).get().get().minSdkVersion
+        } catch (_: Exception) {
+            null
+        }
+    }
