d521f017fabb174c08715bc51dbe302f556902b3
abeade <albert.beade@gmail.com>
2023-10-23 18:40:28 +0200

    Recover util build

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ tasks {
     patchPluginXml {
         version = properties("pluginVersion")
         sinceBuild = properties("pluginSinceBuild")
-        // untilBuild = properties("pluginUntilBuild")
+        untilBuild = properties("pluginUntilBuild")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
